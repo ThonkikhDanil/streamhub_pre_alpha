6b7fa8f89e157ce43d09abf605d2cc1330854811
ThonkikhDanil <daniltonkih575@outlook.com>
2025-02-01 16:48:21 +0500

    Added ApplicationLogo component

--- resources/js/Components/Header.vue
+++ resources/js/Components/Header.vue
@@ -1,5 +1,5 @@
 <script setup>
-	import { Link } from '@inertiajs/vue3';
+	import ApplicationLogo from '@/Components/ApplicationLogo.vue';
 </script>
 
 <template>
@@ -7,7 +7,7 @@
 		<div class="flex items-center justify-between w-72 text-[#010326] dark:text-[#D5F2F2] text-2xl">
 			<i class="relative fi fi-br-menu-burger top-1" />
 			<i class="relative fi fi-br-brightness top-1" />
-			<Link :href="route('welcome')" class="text-[#010326] dark:text-[#D5F2F2] font-semibold text-4xl">Stream<span class="text-[#116EBF]">Hub</span></Link>
+			<ApplicationLogo />
 		</div>
 	</div>
 </template>
