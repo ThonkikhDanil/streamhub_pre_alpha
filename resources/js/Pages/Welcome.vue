b0a1c4c9fd1554c025ae0e9ca222a47cedddeaef
ThonkikhDanil <daniltonkih575@outlook.com>
2025-02-01 14:06:56 +0500

    Start position

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,17 @@
+<script setup>
+	import Layout from '@/Layouts/Layout.vue';
+	import { Head } from '@inertiajs/vue3';
+
+	const props = defineProps({
+		title: {
+			type: String,
+		},
+	})
+</script>
+
+<template>
+	<Head :title="title" />
+	<Layout>
+		<p>Hello World!</p>
+	</Layout>
+</template>
