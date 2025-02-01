f28aa854cfcf618f7a709e68b7cc26cf2d6f6687
ThonkikhDanil <daniltonkih575@outlook.com>
2025-02-01 19:40:35 +0500

    Added SeachRow component

--- resources/js/Components/Header.vue
+++ resources/js/Components/Header.vue
@@ -1,6 +1,7 @@
 <script setup>
 	import ApplicationLogo from '@/Components/ApplicationLogo.vue';
 	import ToggleDarkThemeButton from '@/Components/ToggleDarkThemeButton.vue';
+	import SearchRow from '@/Components/SearchRow.vue';
 </script>
 
 <template>
@@ -10,14 +11,7 @@
 			<ToggleDarkThemeButton />
 			<ApplicationLogo />
 		</div>
-		<form action="" class="flex">
-			<input type="text" class="w-96 bg-[#D5F2F2] dark:bg-[#010326] text-[#010326] dark:text-[#D5F2F2] rounded-s-2xl border-2 border-[#202020] border-r-0" placeholder="Введите запрос" />
-			<button type="submit" class="bg-[#010326] dark:bg-[#D5F2F2] hover:bg-[#116EBF] dark:hover:bg-[#1F82BF] transition-all text-2xl py-1 px-8 rounded-e-2xl">
-				<span class="text-[#D5F2F2] dark:text-[#010326] relative top-1">
-					<i class="fi fi-br-search" />
-				</span>
-			</button>
-		</form>
+		<SearchRow />
 		<button class="py-1 px-4 text-[#010326] dark:text-[#D5F2F2] flex items-center rounded-2xl hover:bg-[#116EBF] dark:hover:bg-[#1F82BF] hover:text-[#D5F2F2] dark:hover:text-[#010326] transition-all">
 			<div class="relative mr-4 text-2xl top-1">
 				<i class="fi fi-br-user" />
