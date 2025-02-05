9b4051f9b34390a12c8fea90d787c7fff340fea1
ThonkikhDanil <daniltonkih575@outlook.com>
2025-02-05 16:56:54 +0500

    Added Sidebar component and Subscriptions page

---
+++ resources/js/Components/Sidebar.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+	import NavLink from '@/Components/NavLink.vue';
+</script>
+
+<template>
+	<div class="relative">
+		<div class="absolute flex">
+			<div class="bg-[#D5F2F2] dark:bg-[#010326] h-screen flex flex-col items-center">
+				<NavLink :href="route('welcome')">Главная</NavLink>
+				<NavLink :href="route('subscriptions')">Подписки</NavLink>
+			</div>
+		</div>
+	</div>
+</template>
--- resources/js/Layouts/Layout.vue
+++ resources/js/Layouts/Layout.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 	import Header from '@/Components/Header.vue';
+	import Sidebar from '@/Components/Sidebar.vue';
 </script>
 
 <template>
 	<Header />
+	<Sidebar />
 	<slot />
 </template>
